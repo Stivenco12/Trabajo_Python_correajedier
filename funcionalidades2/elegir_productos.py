87cbc961d8829a2e0dc176f02edb87f96724f97c
Stivenco12 <153353269+Stivenco12@users.noreply.github.com>
2024-08-09 16:30:09 -0500

    test: :fire: se removieron archivos inutiles
    muchos errores en json
    
    en arreglo

--- funcionalidades2/elegir_productos.py
+++ funcionalidades2/elegir_productos.py
@@ -1,4 +1,9 @@
+import os
 import json
+from os import system
+import sys 
+import json
+from enum import Enum
 def panaderia(op):
     print (  "Pan de Bono: 700")
     print ("Pan de Queso: 800")   
@@ -10,44 +15,68 @@ def panaderia(op):
     print("Pan relleno de Arequipe: 1150")
     print("Pan con Salchicha: 1300")
     print("Pan recubierto de Chocolate: 1200")
-    input ("nombra lo que quieres comprar = ")
+
     print ("añadido a la lista de productos")
+    pausar_pantalla
     menuprincipal(1)
 
-def pasteleria(op):
-    bebidas: []
-                "Coca-Cola": 3000,
-        "Pepsi": 2900,
-        "Red-Bull": 4000,
-        "Gatorade": 3700,
-        "Budweiser": 3200,
-        "Hit": 2500,
-        "Pony-Malta": 2300,
-        "Sprite": 3200,
-        "Monster": 3000,
-        "Tropicana": 2400,
+def pasteleria(op):  
+    print (  "Pastel de Vainilla: 5000")
+    print ("Pastel de Chocolate: 5500")   
+    print ("Pastel de bodas: 10000 ")   
+    print( "Glaseado de Vainilla: 3000")
+    print("Glaseado de Chocolate: 3500 ")
+    print("Rollito de Sal: 900")
+    print("Pastel de Arequipe: 5700 ")
+    print("Pastel de Oreo: 7000 ")
+    print("Postre de Limon: 4300, ")
+    print("Postre de Vainilla: 4000," )
     
-    bebidas
+    print ("añadido a la lista de productos")
+    pausar_pantalla
+    menuprincipal(1)
 
+def bebidas(op):
+    print("Coca-Cola: 3000")
+    print("Pepsi: 2900")
+    print("Red-Bull: 4000")
+    print("Gatorade: 3700")
+    print("Budweiser: 3200")
+    print("Hit: 2500")
+    print("Pony-Malta: 2300")
+    print("Sprite: 3200")
+    print("Monster: 3000")
+    print("Tropicana: 2400")
+    print("-Promociones-: Algunas promociones de la sección de Bebidas")
+    print("1 Pan con Salchicha y 1 Pony Malta: 3000")
+    print("1 Postre de Oreo y 1 Budweiser: 9000")
+    
+    print ("añadido a la lista de productos")
+    pausar_pantalla
+    menuprincipal(1)
 
+def apartado_promocione(op):
+    print("6 Panes Integrales: 4000")
+    print("5 Panes recubiertos de Chocolate: 5500")
+    print("3 Pasteles de Vainilla: 13000")
+    print("4 Postres de Oreo: 25000")
+    print("1 Pan con Salchicha y 1 Pony Malta: 3000")
+    print("1 Postre de Oreo y 1 Budweiser: 9000")
 
+    print ("añadido a la lista de productos")
+    pausar_pantalla
+    menuprincipal(1)
 
+def pausar_pantalla():
+    if sys.platform == "Linux" or sys.platform == "darwin":
+        x=input("presione una tecla para continuar")
+    else:
+        system("pause")
 
-
-
-
-
-
-
-
-
-
-
-import os
 def menuprincipal(op):
     os.system("cls")
     menuprincipalop = "n\1 padaneria \n2 pasteleria \n3 bebidas "
-    if (op !=3):
+    if (op !=4):
         print ("elije que quieres comprar")
         print (menuprincipalop)
         try:
@@ -59,7 +88,27 @@ def menuprincipal(op):
             match (opcion):
                 case 1:
                     panaderia(1)
+                    
+                    panaderia_producto=input ("nombra lo que quieres comprar = ")
                 case 2:
                     pasteleria(1)
+                    pausar_pantalla
+                    pasteleria_produc=input ("nombra lo que quieres comprar = ")
                 case 3:
-                    print()
+                    bebidas(1)
+                    pausar_pantalla
+                    bebidas_produc = input ( "nombra lo que quieres comprar = " )
+                case 4:
+                    apartado_promocione(1)
+                    pausar_pantalla
+                    promocies_produc = input ( "nombra lo que quieres comprar = " )
+
+    
+    data1 = {
+        'produc1' : panaderia_producto,
+        'produc2' : pasteleria_produc,
+        'produc3' : bebidas_produc,
+        'produc4' : promocies_produc,
+    }
+    with open('guardar_informacion.json', 'w') as jf: 
+        json.dump(data1, jf, ensure_ascii=False, indent=2)
